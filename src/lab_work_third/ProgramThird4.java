5ddb1a0621a36c88014c105dc38a549ce0cf9074
Demon1997rus <118682916+Demon1997rus@users.noreply.github.com>
2023-04-03 18:08:55 +0400

    3 Лаба по ООП

---
+++ src/lab_work_third/ProgramThird4.java
@@ -0,0 +1,99 @@
+package lab_work_third;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JSpinner;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+import javax.swing.border.BevelBorder;
+import java.awt.Font;
+
+public class ProgramThird4 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProgramThird4 window = new ProgramThird4();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ProgramThird4() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = 	new JFrame();
+		frame.setBounds(100, 100, 657, 508);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBounds(10, 11, 489, 447);
+		frame.getContentPane().add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Consolas", Font.PLAIN, 14));
+		panel.add(textArea, BorderLayout.CENTER);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(509, 32, 122, 20);
+		frame.getContentPane().add(spinner);
+		
+		JLabel lblNewLabel = new JLabel("Размер фигуры");
+		lblNewLabel.setBounds(509, 7, 122, 27);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Отобразить");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				int n = (int)spinner.getValue();
+				String text = "";
+				int i = 1;
+				int number = n;
+				do {
+					int j = i;
+					do {
+						text += number + "\t";
+						number--;
+						j++;
+					} while(j <= n);
+					number = n; 
+					text += "\n";
+					i++;
+				} while(i <= n);
+				textArea.setText(text);
+			}
+		});
+		btnNewButton.setBounds(509, 63, 122, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
